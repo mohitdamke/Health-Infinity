963473124481ea2877dea08ba605e4cee355d5e6
raees <mohitdamke1@gmail.com>
2025-12-18 20:25:17 +0530

    Add background signal data generator using Kotlin Flow

--- app/src/main/java/mohit/dev/healthinfinity/data/SignalDataSource.kt
+++ app/src/main/java/mohit/dev/healthinfinity/data/SignalDataSource.kt
@@ -1,6 +1,17 @@
 package mohit.dev.healthinfinity.data
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.random.Random
+
 class SignalDataSource {
 
+    fun signalFlow(): Flow<Int> = flow {
+        while (true) {
+            emit(Random.nextInt(0, 101))
+            delay(100)
+        }
+    }
 
 }
